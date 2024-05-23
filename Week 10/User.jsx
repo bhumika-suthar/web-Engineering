84fd03d657f7fd8db63cd490824764a4dcdb3410
Bhomika Kumari <112164463+bhumika-suthar@users.noreply.github.com>
2024-05-23 23:26:17 +0500

    Add files via upload

---
+++ Week 10/User.jsx
@@ -0,0 +1,25 @@
+// props: way to receive data
+export const User = ({
+  // default props
+  username = "Loki",
+  email = "Email@gmail.com",
+  address = { city: "Custom City", zipcode: "0011" },
+  index,
+}) => {
+  return (
+    <div class="user">
+      <img
+        src={`https://api.dicebear.com/8.x/lorelei/svg?seed=${username}`}
+        alt="user"
+      />
+      <div>{username}</div>
+      <div>{email}</div>
+      <div>
+        Address :{" "}
+        <span>
+          {address?.city || "Custom City"}, ({address?.zipcode || "0011"})
+        </span>
+      </div>
+    </div>
+  );
+};
